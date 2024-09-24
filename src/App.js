f0f22ef79150abcbcfc4cdb71e205584ee4f850e
Irshanac <irshana951@gmail.com>
2024-09-24 17:12:13 +0430

    router and HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import First from "./components/HOC/First";
-import Heading from "./components/HOC/Heading";
+import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
+import Home from './components/router/Home'; // Make sure the path to Home.js is correct
+import About from './components/router/About';
 function App() {
   return (
-    <div >
-      <First/>
-      <Heading/>
-     </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+  
   );
 }
 
---
+++ src/components/HOC/router/Home.js
@@ -0,0 +1,6 @@
+const Home=()=>{
+    return (
+        <h1>hi it is Home page</h1>
+    )
+}
+export default Home
